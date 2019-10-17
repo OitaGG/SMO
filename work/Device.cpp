853c4a313ec39be2ebb16e6a5b751ee698b37fea
oitag <oitag@yandex.ru>
2019-10-17 22:35:30 +0300

    Fix recieving from buffer to device error

--- work/Device.cpp
+++ work/Device.cpp
@@ -14,26 +14,26 @@ Device::Device(TimeManager* timeManager, Buffer* buffer, int N) : timeManager_(t
 void Device::get(int i){
   for(size_t k = 0; k < i; k++){
     int numS = buffer_->get();
-    std::cout<<numS<<std::endl;
     if(numS == -1)
       return;
     std::cout<<"Buffer sent to device from Source: "<<numS<<std::endl;
     timeManager_->sent(numS);
     int place = recievePlace();
-    std::cout<<"Place in device: "<<place<<std::endl;
     devicesArray_[place] = numS;
     wait_[place] = time_ + fxRule();
     timeManager_->addNewTime(wait_[place]);
-    std::cout<<wait_[place]<<std::endl;
+    getFreePlaces();
   }
 }
 
 int Device::getFreePlaces(){
   int count = 0;
-  for(int i = 0; i < amount_; i ++){
-    if(devicesArray_[i] = -1)
+  for(int i = 0; i < amount_; i++){
+    if(devicesArray_[i] == -1){
       count++;
+    }
   }
+  std::cout<<"Free dev: "<<count<<std::endl;
   return count;
 }
 
@@ -43,7 +43,6 @@ void Device::work(){
   std::cout<<"******************"<<std::endl;
   free();//Освобождает готовое
   int freePlaces = getFreePlaces();
-  std::cout<<"Devices free now: "<< freePlaces <<std::endl;
   get(freePlaces);//Смотрит, сколько можно взять и вставить в девайс
   std::cout<<"Succesfully iteration for Device"<<std::endl;  
   std::cout<<"******************"<<std::endl;
@@ -66,10 +65,13 @@ double Device::fxRule(){
 
 int Device::recievePlace(){
   int place = -1;
-  for (size_t i = amount_; i > 0; i--){
-    if(devicesArray_[i] != -1)
+  for (size_t i = 0; i < amount_; i++){
+    if(devicesArray_[i] == -1){
       place = i;
+      break;
+    }
   }
+  std::cout<<"ANNA-LOH: "<< place <<std::endl;
   return place;
 }
 Device::~Device(){
